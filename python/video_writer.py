b724edfa0d89ec2cb574baf2d0b9214f7c945c55
Julian Panetta <julian.panetta@gmail.com>
2020-10-13 23:33:22 -0700

    "Streaming" mode for video recorder that writes frames to disk as soon as possible (essentially after every keyframe)

--- python/video_writer.py
+++ python/video_writer.py
@@ -12,7 +12,11 @@ class VideoWriter:
     """
     Writes a raw image stream to an image sequence or a compressed video
     """
-    def __init__(self, outPath, inWidth, inHeight, codec=Codec.H264, framerate=30, quality='-crf 23', outWidth=None, outHeight=None):
+    def __init__(self, outPath, inWidth, inHeight, codec=Codec.H264, framerate=30, quality='-crf 23', streaming=False, outWidth=None, outHeight=None, additionalFlags=[]):
+        """
+        streaming - Whether to write a fragmented MP4 that can be watched as it
+        is being written (and is usable if the writing job dies before it finishes).
+        """
         self. outPath     = outPath
         self. inWidth     = inWidth
         self. inHeight    = inHeight
@@ -28,6 +32,8 @@ class VideoWriter:
             os.makedirs(outPath, exist_ok=True)
         else:
             ffmpegCommand = ['ffmpeg', '-y']
+            if streaming:
+                ffmpegCommand += ['-probesize', '32', '-flags', 'low_delay']
             # Input settings (read raw pixel data from stdin)
             ffmpegCommand += [
                     '-f', 'rawvideo', '-pixel_format', 'rgba',
@@ -40,8 +46,19 @@ class VideoWriter:
                 ffmpegCommand += ['-vf', f'scale={self.outWidth}:{self.outHeight}']
             ffmpegCommand += ['-vcodec'] + codec.value # Codec CLI settings are stored in enum
             ffmpegCommand += quality.split()
+            if streaming:
+                ffmpegCommand += ['-g', '15', '-movflags', 'frag_keyframe+empty_moov', '-blocksize', '2048', '-flush_packets', '1']
+                ffmpegCommand += ['-tune', 'zerolatency']
+                # Alternatively, we could use the subset of the x264 options selected by `-tune zerolatency` that are needed to ensure
+                # the video file on disk is updated every "group of pictures" frames (15 here). The full list of options is:
+                #       '--bframes 0 --force-cfr --no-mbtree --sync-lookahead 0 --sliced-threads --rc-lookahead 0'
+                # of these, `--bframes 0` is not as crucial, but does seem to reduce the frame latency by
+                # by about 5 frames without substantially increasing file size.
+                # ffmpegCommand += ['-x264opts', 'force-cfr=1:no-mbtree=1:sliced-threads=1:sync-lookahead=0:rc-lookahead=0']
+            ffmpegCommand.extend(additionalFlags)
             ffmpegCommand.append(outPath)
 
+            # print(f"ffmpeg command: {' '.join(ffmpegCommand)}")
             self.ffmpegProc = sp.Popen(ffmpegCommand, stdin=sp.PIPE)
 
     def frameDataSize(self):
@@ -78,8 +95,8 @@ class ContextVideoWriter(VideoWriter):
     """
     Renders an OpenGL context to an image sequence or a compressed video
     """
-    def __init__(self, outPath, ctx, codec=Codec.H264, framerate=30, outWidth=None, outHeight=None):
-        super().__init__(outPath, ctx.width, ctx.height, codec=codec, framerate=framerate, outWidth=outWidth, outHeight=outHeight)
+    def __init__(self, outPath, ctx, codec=Codec.H264, framerate=30, streaming=False, outWidth=None, outHeight=None):
+        super().__init__(outPath, ctx.width, ctx.height, codec=codec, framerate=framerate, streaming=streaming, outWidth=outWidth, outHeight=outHeight)
         self.ctx = ctx
 
     def writeFrame(self):
@@ -89,8 +106,8 @@ class MeshRendererVideoWriter(VideoWriter):
     """
     Creates an image sequence or a compressed video from  a MeshRenderer
     """
-    def __init__(self, outPath, mrenderer, codec=Codec.H264, framerate=30, outWidth=None, outHeight=None):
-        super().__init__(outPath, mrenderer.ctx.width, mrenderer.ctx.height, codec=codec, framerate=framerate, outWidth=outWidth, outHeight=outHeight)
+    def __init__(self, outPath, mrenderer, codec=Codec.H264, framerate=30, streaming=False, outWidth=None, outHeight=None):
+        super().__init__(outPath, mrenderer.ctx.width, mrenderer.ctx.height, codec=codec, framerate=framerate, streaming=streaming, outWidth=outWidth, outHeight=outHeight)
         self.mrenderer = mrenderer
         if codec != Codec.ImgSeq:
             # FFmpeg doesn't support transparent H264/HEVC output--it just
@@ -109,13 +126,13 @@ class PlotVideoWriter(VideoWriter):
     """
     Creates an image sequence or a compressed video from  a MeshRenderer
     """
-    def __init__(self, outPath, fig, dpi = 72, codec=Codec.H264, framerate=30, outWidth=None, outHeight=None):
+    def __init__(self, outPath, fig, dpi = 72, codec=Codec.H264, framerate=30, streaming=False, outWidth=None, outHeight=None):
         fig.set_dpi(dpi)
         self.dpi = dpi
         fig.tight_layout()
         fig.canvas.draw()
         fig.canvas.buffer_rgba().tobytes()
-        super().__init__(outPath, *fig.canvas.get_width_height(), codec=codec, framerate=framerate, outWidth=outWidth, outHeight=outHeight)
+        super().__init__(outPath, *fig.canvas.get_width_height(), codec=codec, framerate=framerate, streaming=streaming, outWidth=outWidth, outHeight=outHeight)
 
     def writeFrame(self, fig):
         """
@@ -132,3 +149,16 @@ class PlotVideoWriter(VideoWriter):
         if (w != self.inWidth) or (h != self.inHeight): raise Exception('Plot dimensions changed')
         fig.canvas.draw()
         super().writeFrame(np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8))
+
+def countFrames(vidPath):
+    """
+    Count the number of frames in video file `vidPath`.
+    """
+    p = sp.Popen(['ffmpeg', '-i', vidPath, '-map', '0:v:0', '-c', 'copy', '-f', 'null', '-'], stderr=sp.PIPE)
+    out, err = p.communicate()
+    for l in err.split(b'\n'):
+        tokens = l.split()
+        if len(tokens) == 0: continue
+        if tokens[0] == b'frame=':
+            return int(tokens[1])
+    return -1
